5136b70184e2d64ccaa809abd353a4f1a089a73b
Todd Volkert <tvolkert@google.com>
2020-07-23 19:03:56 -0700

    SaveInvoice updates
    * Updated to use ContextAction
    * Updated to use TaskMonitor

--- apps/payouts/lib/src/actions/save_invoice.dart
+++ apps/payouts/lib/src/actions/save_invoice.dart
@@ -4,6 +4,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
 
 import 'package:payouts/src/pivot.dart' as pivot;
+import 'package:payouts/ui/common/task_monitor.dart';
 
 class SaveInvoiceIntent extends Intent {
   const SaveInvoiceIntent({this.context});
@@ -11,7 +12,7 @@ class SaveInvoiceIntent extends Intent {
   final BuildContext context;
 }
 
-class SaveInvoiceAction extends Action<SaveInvoiceIntent> {
+class SaveInvoiceAction extends ContextAction<SaveInvoiceIntent> {
   @override
   bool isEnabled(SaveInvoiceIntent intent) {
     // TODO(tvolkert): switch enabled based on whether invoice needs saving.
@@ -19,14 +20,31 @@ class SaveInvoiceAction extends Action<SaveInvoiceIntent> {
   }
 
   @override
-  Future<void> invoke(SaveInvoiceIntent intent) async {
-    BuildContext context = intent.context ?? primaryFocus.context;
+  Future<void> invoke(SaveInvoiceIntent intent, [BuildContext context]) async {
+    context ??= intent.context ?? primaryFocus.context;
     if (context == null) {
       throw StateError('No context in which to invoke $runtimeType');
     }
 
-    await SaveInvoiceSheet.open(context: context);
     print('TODO: save invoice');
+    bool doError = true;
+    final Future<int> saveInvoiceFuture = Future<int>.delayed(const Duration(seconds: 2), () {
+      if (doError) {
+        throw StateError('uh oh super now I have to deal with trying to read a really long error message');
+      }
+      return 15;
+    });
+
+    try {
+      int value = await TaskMonitor.of(context).monitor<int>(
+        future: saveInvoiceFuture,
+        inProgressMessage: 'Saving invoice...',
+        completedMessage: 'Invoice saved',
+      );
+      print('value in my app: $value');
+    } catch (error) {
+      print('error in my app: $error');
+    }
   }
 }
 
