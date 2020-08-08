7f6e0a60fb802faa76cb3bfc4a20b44d6a4117ed
Todd Volkert <tvolkert@google.com>
2020-08-07 17:10:09 -0700

    Updates for table view
    * Add RenderSegment and SegmentConstraints
    * Update ScrollPane to be segment-aware
    * Make RenderScrollPane a repaint boundary
    * Add BasicTableView

---
+++ apps/payouts/lib/src/pivot/segment.dart
@@ -0,0 +1,71 @@
+import 'package:flutter/foundation.dart';
+import 'package:flutter/rendering.dart';
+
+class SegmentConstraints extends BoxConstraints {
+  const SegmentConstraints({
+    double minWidth = 0,
+    double maxWidth = double.infinity,
+    double minHeight = 0,
+    double maxHeight = double.infinity,
+    this.viewport,
+  }) : super(minWidth: minWidth, maxWidth: maxWidth, minHeight: minHeight, maxHeight: maxHeight);
+
+  SegmentConstraints.fromBoxConstraints({
+    BoxConstraints boxConstraints,
+    this.viewport,
+  }) : super(
+          minWidth: boxConstraints.minWidth,
+          maxWidth: boxConstraints.maxWidth,
+          minHeight: boxConstraints.minHeight,
+          maxHeight: boxConstraints.maxHeight,
+        );
+
+  final Rect viewport;
+
+  BoxConstraints asBoxConstraints() {
+    return BoxConstraints(
+      minWidth: minWidth,
+      maxWidth: maxWidth,
+      minHeight: minHeight,
+      maxHeight: maxHeight,
+    );
+  }
+
+  @override
+  bool operator ==(Object other) {
+    if (identical(this, other))
+      return true;
+    if (!(super == (other)))
+      return false;
+    return other is SegmentConstraints && other.viewport == viewport;
+  }
+
+  @override
+  int get hashCode {
+    assert(debugAssertIsValid());
+    return hashValues(super.hashCode, viewport);
+  }
+
+  @override
+  String toString() {
+    return 'SegmentConstraints(base=${super.toString()}, viewport=$viewport)';
+  }
+}
+
+abstract class RenderSegment extends RenderBox {
+  @override
+  SegmentConstraints get constraints {
+    final BoxConstraints constraints = super.constraints;
+    assert(() {
+      if (constraints is! SegmentConstraints) {
+        FlutterError.reportError(FlutterErrorDetails(
+          exception: 'RenderSegment was given constraints other than SegmentConstraints',
+          stack: StackTrace.current,
+          library: 'pivot',
+        ));
+      }
+      return true;
+    }());
+    return constraints as SegmentConstraints;
+  }
+}
