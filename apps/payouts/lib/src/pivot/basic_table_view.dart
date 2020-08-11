e9002f98788c581b6b112ff45aa7dfb85d37f8fa
Todd Volkert <tvolkert@google.com>
2020-08-10 18:20:26 -0700

    Made constructor const

--- apps/payouts/lib/src/pivot/basic_table_view.dart
+++ apps/payouts/lib/src/pivot/basic_table_view.dart
@@ -28,7 +28,7 @@ typedef TableViewLayoutCallback = void Function({
 
 @immutable
 class BasicTableColumn with Diagnosticable {
-  BasicTableColumn({
+  const BasicTableColumn({
     this.width = const FlexTableColumnWidth(),
     @required this.cellRenderer,
   });
