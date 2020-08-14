f739e003eb95914e2c1b8c1d18c8e7b647196270
Todd Volkert <tvolkert@google.com>
2020-08-14 16:45:01 -0700

    Table view updates.
    * Added support for `debugPaintTableCellBuilds`
    * Added `TableCellRange.where()`, and implemented `subtract` and `intersect`,
      `skipAlreadyBuilt`, `skipOutOfBounds`, and `skipEmptyCells` in terms of
      `where`
    * Added `RenderBasicTableView.markCellsDirty()`, and wired support for it
      into `RenderBasicTableView.rebuildIfNecessary()`. Then made highligh
      handler and column resize handler use this feature for more efficient
      rendering when hovering mouse over cells or resizing columns.
    * Made `Range` mixin `Diagnosticable`

---
+++ apps/payouts/lib/src/pivot/debug.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/painting.dart';
+
+const HSVColor _kDebugDefaultRepaintColor = HSVColor.fromAHSV(0.4, 60.0, 1.0, 1.0);
+
+/// Overlay a rotating set of colors when rebuilding table cells in checked
+/// mode.
+///
+/// See also:
+///
+///  * [debugRepaintRainbowEnabled], a similar flag for visualizing layer
+///    repaints.
+bool debugPaintTableCellBuilds = false;
+
+/// The current color to overlay when repainting a table cell build.
+HSVColor debugCurrentTableCellColor = _kDebugDefaultRepaintColor;
