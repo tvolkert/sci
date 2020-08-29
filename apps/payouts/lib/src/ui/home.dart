bb53f47a3287dd6419f8877c69fe8f1f8edeca64
Todd Volkert <tvolkert@google.com>
2020-08-29 00:02:38 -0700

    Made home widget respond to open invoice

--- apps/payouts/lib/src/ui/home.dart
+++ apps/payouts/lib/src/ui/home.dart
@@ -1,10 +1,38 @@
 import 'package:flutter/material.dart' show Divider;
 import 'package:flutter/widgets.dart';
 
+import 'package:payouts/src/model/invoice.dart';
+
 import 'invoice.dart';
 import 'toolbar.dart';
 
-class PayoutsHome extends StatelessWidget {
+class PayoutsHome extends StatefulWidget {
+  @override
+  _PayoutsHomeState createState() => _PayoutsHomeState();
+}
+
+class _PayoutsHomeState extends State<PayoutsHome> {
+  InvoiceBindingListener _listener;
+
+  void _handleInvoiceChanged(Invoice oldInvoice) {
+    setState(() {});
+  }
+
+  bool get _shouldShowInvoice => InvoiceBinding.instance.invoice != null;
+
+  @override
+  void initState() {
+    super.initState();
+    _listener = InvoiceBindingListener(onInvoiceChanged: _handleInvoiceChanged);
+    InvoiceBinding.instance.addListener(_listener);
+  }
+
+  @override
+  void dispose() {
+    InvoiceBinding.instance.removeListener(_listener);
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Column(
@@ -17,9 +45,20 @@ class PayoutsHome extends StatelessWidget {
           color: Color(0xff999999),
         ),
         Expanded(
-          child: InvoiceView(),
+          child: _shouldShowInvoice ? InvoiceView() : _InvoiceArea(),
         ),
       ],
+    );
+  }
+}
+
+class _InvoiceArea extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return const DecoratedBox(
+      decoration: BoxDecoration(
+        color: Color(0xffc8c8bb)
+      ),
     );
   }
 }
