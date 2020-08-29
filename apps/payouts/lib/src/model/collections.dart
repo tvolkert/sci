b8c0280d2c806212bcfb080ea3724aaddb0238d7
Todd Volkert <tvolkert@google.com>
2020-08-28 17:00:00 -0700

    Added ForwardingIterable

--- apps/payouts/lib/src/model/collections.dart
+++ apps/payouts/lib/src/model/collections.dart
@@ -393,7 +393,7 @@ class NotifyingList<T>
   bool every(bool Function(T element) test) => delegate.every(test);
 
   @override
-  Iterable<R> expand<R>(Iterable<R> Function(T element) f) => delegate.expand(f);
+  Iterable<R> expand<R>(Iterable<R> Function(T element) f) => delegate.expand<R>(f);
 
   @override
   void fillRange(int start, int end, [T fillValue]) {
@@ -409,7 +409,7 @@ class NotifyingList<T>
 
   @override
   R fold<R>(R initialValue, R Function(R previousValue, T element) combine) {
-    return delegate.fold(initialValue, combine);
+    return delegate.fold<R>(initialValue, combine);
   }
 
   @override
@@ -595,6 +595,109 @@ class NotifyingList<T>
     final List<T> newList = delegate.toList(growable: growable);
     return NotifyingList<T>(newList);
   }
+
+  @override
+  Set<T> toSet() => delegate.toSet();
+
+  @override
+  Iterable<T> where(bool Function(T element) test) => delegate.where(test);
+
+  @override
+  Iterable<R> whereType<R>() => delegate.whereType<R>();
+}
+
+mixin ForwardingIterable<T> implements Iterable<T> {
+  @protected
+  Iterable<T> get delegate;
+
+  @override
+  bool any(bool Function(T element) test) => delegate.any(test);
+
+  @override
+  Iterable<R> cast<R>() => Iterable.castFrom<T, R>(delegate);
+
+  @override
+  bool contains(Object element) => delegate.contains(element);
+
+  @override
+  T elementAt(int index) => delegate.elementAt(index);
+
+  @override
+  bool every(bool Function(T element) test) => delegate.every(test);
+
+  @override
+  Iterable<R> expand<R>(Iterable<R> Function(T element) f) => delegate.expand<R>(f);
+
+  @override
+  T get first => delegate.first;
+
+  @override
+  T firstWhere(bool Function(T element) test, {T Function() orElse}) {
+    return delegate.firstWhere(test, orElse: orElse);
+  }
+
+  @override
+  R fold<R>(R initialValue, R Function(R previousValue, T element) combine) {
+    return delegate.fold<R>(initialValue, combine);
+  }
+
+  @override
+  Iterable<T> followedBy(Iterable<T> other) => delegate.followedBy(other);
+
+  @override
+  void forEach(void Function(T element) f) => delegate.forEach(f);
+
+  @override
+  bool get isEmpty => delegate.isEmpty;
+
+  @override
+  bool get isNotEmpty => delegate.isNotEmpty;
+
+  @override
+  Iterator<T> get iterator => delegate.iterator;
+
+  @override
+  String join([String separator = ""]) => delegate.join(separator);
+
+  @override
+  T get last => delegate.last;
+
+  @override
+  T lastWhere(bool Function(T element) test, {T Function() orElse}) {
+    return delegate.lastWhere(test, orElse: orElse);
+  }
+
+  @override
+  int get length => delegate.length;
+
+  @override
+  Iterable<R> map<R>(R Function(T e) f) => delegate.map<R>(f);
+
+  @override
+  T reduce(T Function(T value, T element) combine) => delegate.reduce(combine);
+
+  @override
+  T get single => delegate.single;
+
+  @override
+  T singleWhere(bool Function(T element) test, {T Function() orElse}) {
+    return delegate.singleWhere(test, orElse: orElse);
+  }
+
+  @override
+  Iterable<T> skip(int count) => delegate.skip(count);
+
+  @override
+  Iterable<T> skipWhile(bool Function(T value) test) => delegate.skipWhile(test);
+
+  @override
+  Iterable<T> take(int count) => delegate.take(count);
+
+  @override
+  Iterable<T> takeWhile(bool Function(T value) test) => delegate.takeWhile(test);
+
+  @override
+  List<T> toList({bool growable = true}) => delegate.toList(growable: growable);
 
   @override
   Set<T> toSet() => delegate.toSet();
