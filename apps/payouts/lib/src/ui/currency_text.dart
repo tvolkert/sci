9713b69ed3ccffbb0478a4fd6f3856eb56318b52
Todd Volkert <tvolkert@google.com>
2020-08-30 17:16:09 -0700

    Added currency text widget

---
+++ apps/payouts/lib/src/ui/currency_text.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/widgets.dart';
+
+import 'package:payouts/src/model/constants.dart';
+
+class CurrencyText extends StatelessWidget {
+  const CurrencyText({
+    Key key,
+    this.prefix,
+    this.amount,
+  }) : super(key: key);
+
+  final String prefix;
+  final double amount;
+
+  @override
+  Widget build(BuildContext context) {
+    final StringBuffer buf = StringBuffer();
+    buf.writeAll([
+      if (prefix != null) prefix,
+      NumberFormats.currency.format(amount),
+    ]);
+    return Text(buf.toString(), maxLines: 1, softWrap: false);
+  }
+}
