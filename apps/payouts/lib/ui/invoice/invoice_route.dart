8b4d1ee5ecd6822587ae0aaf3176685116524139
Todd Volkert <tvolkert@google.com>
2020-11-08 23:34:37 -0800

    Deleted experimental (mobile) UI files

--- apps/payouts/lib/ui/invoice/invoice_route.dart
+++
@@ -1,9 +0,0 @@
-// @dart=2.9
-
-import 'package:flutter/material.dart';
-
-class InvoiceRoute extends MaterialPageRoute<void> {
-  InvoiceRoute({Widget child}) : super(builder: (BuildContext context) {
-    return child;
-  });
-}
